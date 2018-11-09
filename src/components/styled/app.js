07b02511be916419778953dda50966e59917d9f2
Donat Travin <dtravin@griddynamics.com>
2018-11-08 16:57:12 -0800

    Refactored project a little

---
+++ src/components/styled/app.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Panel = styled.div`
+  height: 50px;
+  background-image: linear-gradient(to top, rgba(40, 133, 117, 0.13) 0%, rgba(255, 255, 255, 0.13) 100%);
+  background-color: rgb(40, 133, 117);
+  line-height: 50px;
+  z-index: 1000;
+  color: #FFFFFF;
+  font-size: 15px;
+  text-transform: capitalize;
+`;
+
+export const Header = styled(Panel)`
+  border-bottom: 3px solid #58ba96;
+  width: 100%;
+  align-self: flex-start;
+`;
+
+export const Footer = styled(Panel)`
+  width: 100%;
+  text-align: center;
+  align-self: flex-end;
+`;
+
+export const HeaderMenu = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 100%;
+  text-align: right;
+`;
+
+export const Main = styled.main`
+  padding: 0 15px 50px;
+  text-align: left;
+  margin: 0 auto;
+  max-width: 1140px;
+  background: rgba(0,0,0, 0.4);
+  min-height: calc(100% - 153px);
+  flex-grow: 99;
+  min-width: 80%;
+`;
+
+export const BagIcon = styled.i`
+  content: url(${process.env.PUBLIC_URL + '/assets/images/bag-icon.png'});
+  float: left;
+  margin: 13px 30px;
+  cursor: pointer;
+`;
+
+export const LogoutIcon = styled.i`
+  content: url(${process.env.PUBLIC_URL + '/assets/images/logout-icon.png'});
+  vertical-align: middle;
+  margin: 0 30px 0 10px;
+  cursor: pointer;
+`;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,66 +5,11 @@ import { Router, Route } from 'react-router';
 import LoginGuardRoute from '../components/login-guard-route';
 import {history} from "../helpers/history";
 import { CommonLayout } from '../components/styled/common-layout';
-import styled from 'styled-components';
 import connect from "react-redux/es/connect/connect";
 import { logout } from '../actions/authentication';
 import PropTypes from 'prop-types';
 import authService from '../services/authentication';
-
-const Panel = styled.div`
-  height: 50px;
-  background-image: linear-gradient(to top, rgba(40, 133, 117, 0.13) 0%, rgba(255, 255, 255, 0.13) 100%);
-  background-color: rgb(40, 133, 117);
-  line-height: 50px;
-  z-index: 1000;
-  color: #FFFFFF;
-  font-size: 15px;
-  text-transform: capitalize;
-`;
-
-const Header = styled(Panel)`
-  border-bottom: 3px solid #58ba96;
-  width: 100%;
-  align-self: flex-start;
-`;
-
-const Footer = styled(Panel)`
-  width: 100%;
-  text-align: center;
-  align-self: flex-end;
-`;
-
-const HeaderMenu = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  height: 100%;
-  text-align: right;
-`;
-
-const Main = styled.main`
-  padding: 0 15px 50px;
-  text-align: left;
-  margin: 0 auto;
-  max-width: 1140px;
-  background: rgba(0,0,0, 0.4);
-  min-height: calc(100% - 153px);
-  flex-grow: 99;
-  min-width: 80%;
-`;
-
-const BagIcon = styled.i`
-  content: url(${process.env.PUBLIC_URL + '/assets/images/bag-icon.png'});
-  float: left;
-  margin: 13px 30px;
-  cursor: pointer;
-`;
-
-const LogoutIcon = styled.i`
-  content: url(${process.env.PUBLIC_URL + '/assets/images/logout-icon.png'});
-  vertical-align: middle;
-  margin: 0 30px 0 10px;
-  cursor: pointer;
-`;
+import { Header, Footer, HeaderMenu, Main, BagIcon, LogoutIcon } from '../components/styled/app';
 
 const mapStateToProps = (state) => {
     return {
