c8f0df0af587eb0df99145582f3490b00fa63d3d
Donat Travin <dtravin@griddynamics.com>
2018-11-09 16:36:41 -0800

    fixed minor issue

--- src/components/product-list.js
+++ src/components/product-list.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ProductCard from './product-card';
-import ProductList from './styled/product-list';
+import { ProductList } from './styled/product-list';
 
 const ProductsList = (props) => {
     return <ProductList>
