0e185da873817074f86c9658cb5502fae1237107
Donat Travin <dtravin@griddynamics.com>
2018-11-15 22:27:32 -0800

    forgot files

---
+++ src/components/styled/infinite-scroll.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const LoadingOverlayContainer = styled.div`
+    position: relative;
+    width: 100%;
+    height: 60px;
+`;
---
+++ src/components/styled/loading-overlay.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const LoadingOverlayContainer = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+`;
+
+export const SpinnerContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+export const Spinner = styled.div`
+    border: 5px solid #fdfdfd;
+    border-radius: 50%;
+    border-top: 5px solid #37b7b6;
+    width: 30px;
+    height: 30px;
+    animation: spin 2s linear infinite;
+    
+    @keyframes spin {
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+    }
+`;
+
+export const ShadowOverlay = styled.div`
+    background: #0000004a;
+    width: 100%;
+    height: 100%;
+`;
